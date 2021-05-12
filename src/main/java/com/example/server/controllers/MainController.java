2219a3a1f63ef0e9e50f1dd320d51d4c4a4c67d0
Amir Benassayag <amir12061968@gmail.com>
2021-05-12 23:20:38 +0300

    Change route to breeds

--- src/main/java/com/example/server/controllers/MainController.java
+++ src/main/java/com/example/server/controllers/MainController.java
@@ -6,14 +6,12 @@ import com.example.server.data.Types;
 import com.example.server.helpers.AllPetsHelper;
 import com.example.server.helpers.AllTypesHelper;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 
 @RequestMapping("/api")
@@ -40,11 +38,14 @@ public class MainController {
         }
     }
 
-    @GetMapping("/petTypes")
-    public Types getTypes(){
-        String url= this.EXTERNAL_API+ "types";
+    @GetMapping("/{type}/breeds")
+    public Types getTypes(@PathVariable("type") String type){
+        String url= this.EXTERNAL_API+ "types/{type}/breeds";
+
+        Map<String, String> uriParam = new HashMap<>();
+        uriParam.put("type", type);
         try{
-            String responseBody = restTemplate.exchange(url, HttpMethod.GET, this.entity, String.class).getBody();
+            String responseBody = restTemplate.exchange(url, HttpMethod.GET, this.entity, String.class,uriParam).getBody();
             return AllTypesHelper.getAllTypesAccordingHttpResponse(responseBody);
         }
         catch(Exception e){
--- src/main/java/com/example/server/helpers/AllTypesHelper.java
+++ src/main/java/com/example/server/helpers/AllTypesHelper.java
@@ -11,13 +11,13 @@ import java.util.ArrayList;
 
 public class AllTypesHelper {
 
-    private static final String TYPES= "types";
+    private static final String BREEDS= "breeds";
     private static final String TYPE_NAME="name";
     public static Types getAllTypesAccordingHttpResponse(String externalResponse) throws JsonProcessingException {
         ObjectMapper mapper= new ObjectMapper();
         JsonNode node= mapper.readTree(externalResponse);
 
-        JsonNode typesNodes= node.get(TYPES);
+        JsonNode typesNodes= node.get(BREEDS);
         ArrayList<String>types= new ArrayList<>();
         for(JsonNode typeNode: typesNodes){
             String name= typeNode.get(TYPE_NAME).asText();
